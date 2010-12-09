be1edb6ff3577f2ee043b37524dc9a845d0c0cb3
Shane Daniel <simrpgman@gmail.com>
2010-12-08 20:52:26 -0600

    added third party goog.dom.query library for parsing css selectors; separated requirements into own script

---
+++ js/requirements.js
@@ -0,0 +1,13 @@
+goog.provide('sim.siftr');
+goog.provide('sim.array');
+
+goog.require('goog.net.Jsonp');
+goog.require('goog.dom');
+goog.require('goog.dom.query');
+goog.require('goog.array');
+goog.require('goog.events');
+goog.require('goog.events.EventTarget');
+goog.require('goog.events.EventType');
+goog.require('goog.fx.DragListGroup');
+goog.require('goog.fx.DragListDirection');
+goog.require('goog.ui.LabelInput');
